47ce1dd64aeee6bcb84e9451ead604b93fe8cf1e
web-vlad <vladmir04@gmail.com>
2019-02-12 22:24:10 +0200

    Begin Service page end

--- src/js/common.js
+++ src/js/common.js
@@ -82,15 +82,83 @@ $(document).ready(function() {
     });
 
     // TAB
-    $(function() {
+    $('.tabs-caption').on('click', '.tabs-caption__item:not(.active)', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('.tabs').parent().parent().find('.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
+            $('.slider-product').slick("reinit");
+    });
 
-        $('.tabs-caption').on('click', '.tabs-caption__item:not(.active)', function() {
-            $(this)
-                .addClass('active').siblings().removeClass('active')
-                .closest('.tabs').parent().parent().find('.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
-                $('.slider-product').slick("reinit");
-        });
+    // SLIDER REFERENCE
+    $('.slider-reference').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        //fade: true,
+        //variableWidth: true,
+        speed: 1500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        //arrows: false,
+        dots: false,
+        //centerMode: true,
+        //centerPadding: '4px',
+        /*appendArrows: $('.b-nav'),
+         prevArrow: '<button id="prev" type="button" class="slick-arrow--prev"></button>',
+         nextArrow: '<button id="next" type="button" class="slick-arrow--next"></button>',*/
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
+    // SLIDER PARTNERS
+    $('.slider-partner').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        //fade: true,
+        //variableWidth: true,
+        speed: 1500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        //arrows: false,
+        dots: false,
+        //centerMode: true,
+        //centerPadding: '4px',
+        /*appendArrows: $('.b-nav'),
+         prevArrow: '<button id="prev" type="button" class="slick-arrow--prev"></button>',
+         nextArrow: '<button id="next" type="button" class="slick-arrow--next"></button>',*/
+        responsive: [
+            /*{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+            },*/
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 421,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
     });
 
 });
